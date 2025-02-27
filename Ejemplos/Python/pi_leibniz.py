bef9ff7b883f3ef1b96bc3a0a48126fe8346b9a0
RichxrdPino <ricardojosepino2@gmail.com>
2025-02-27 12:08:47 -0500

    arreglar error en cálculo de pi de leibniz

--- Ejemplos/Python/pi_leibniz.py
+++ Ejemplos/Python/pi_leibniz.py
@@ -2,7 +2,7 @@ nmax = int(input())
 suma = 0
 k = 0
 while k < nmax:
-    termino = -1 ** k / 2 * k + 1
+    termino = ((-1) ** k) / (2 * k + 1)
     suma = suma + termino
     k = k + 1
 print(suma * 4)
