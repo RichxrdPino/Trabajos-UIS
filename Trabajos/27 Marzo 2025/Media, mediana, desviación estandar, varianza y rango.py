5fd8ac48e35a8a3fe2d27344de2daf83aecdd0cc
Ricardo Pino <119005597+RichxrdPino@users.noreply.github.com>
2025-03-27 16:44:40 +0000

    trabajo en clase

--- Trabajos/27 Marzo 2025/Media, mediana, desviación estandar, varianza y rango.py
+++ Trabajos/27 Marzo 2025/Media, mediana, desviación estandar, varianza y rango.py
@@ -1,33 +1,79 @@
+## Crear un algoritmo con funciones que determine la media, mediana, 
+## varianza, variación desviación estandar y rango de un conjunto de datos.
+
+# LIBRERÍAS
 import random as rnd
 
-n = int(input("Ingresa el número de elementos aleatorios a los que quieres aplicar regresión lineal: "))
+# Solicitar cantidad de datos y definir lista de estos
+n = int(input("Ingresa el número de datos aleatorios a los que quieres aplicar regresión lineal: "))
 A = []
 
-# Generar n elementos en la lista A
+#*  Generar n elementos en la lista A
 for i in range(n):
     A.append(rnd.randint(0,100))
+    
+#* Definir las funciones para calcular cada Valor estadístico
+
+#* Media / Promedio
+def media(datos):
+    media = sum(datos) / len(datos)
+    return media
+
+#* Mediana / Punto medio de los elementos ordenados de menor a mayor
+def mediana(datos):
+    
+    #Ordenar la lista
+    for j in range(len(datos)):
+        for i in range(len(datos)-1):
+            if datos[i] > datos[i+1]:
+                a = datos[i]
+                datos[i] = datos[i+1]
+                datos[i+1] = a
+    
+    print(datos)
+    
+    # Comprobar si la cantidad de elementos es par
+    if n % 2 == 0:
+        mediana = datos[int(n/2)-1] # Si sí, guardar el elemento del medio mayor
+    else:
+        mediana = datos[int((n+1)/2)-1] # Si no, guardar el elemento del medio
+    
+    return mediana
+
+#* Varianza / Dispersión 1
+def varianza(datos):
+    
+    sum = 0
+    for i in datos:
+        sum = sum + ((i - media(datos))**2)
+    varianza = sum / (len(datos) - 1)
+    
+    return varianza
+
+#* Desviación estandar
+def desviacion(datos):
+    desviacion = varianza(datos)**(1/2)
+    
+    return desviacion
+
+def rango(datos):
+    rango = max(datos) - min(datos)
+    
+    return rango
 
 # Comprobar que se generaron elementos
 if len(A) == 0:
     print("|| ERORR || -- Debes pedir generar al menos 1 elemento")
 else:
-    #* Media / Promedio
-    media = sum(A) / len(A)
     
-    #* Mediana / Punto medio de los elementos ordenados de menor a mayor
-    
-    #Ordenar la lista
-    for j in range(len(A)):
-        for i in range(len(A)-1):
-            if A[i] > A[i+1]:
-                a = A[i]
-                A[i] = A[i+1]
-                A[i+1] = a
-    
-    # Comprobar si la cantidad de elementos es par
-    if n % 2 == 0:
-        mediana = A[int(n/2)-1] # Si sí, guardar el elemento del medio mayor
-    else:
-        mediana = A[int((n+1)/2)-1] # Si no, guardar el elemento del medio
-    
-    #* 
+    print(A)
+    print("")
+    print(f"Media: {media(A)}")
+    print("")
+    print(f"Mediana: {mediana(A)}")
+    print("")
+    print(f"Varianza: {varianza(A)}")
+    print("")
+    print(f"Desviación estandar: {desviacion(A)}")
+    print("")
+    print(f"Rango: {rango(A)}")
