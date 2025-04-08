18fe9fcc935ce5fb86e44a842cd113fbdb435a9f
Ricardo Pino <119005597+RichxrdPino@users.noreply.github.com>
2025-04-08 10:42:22 -0500

    Add files via upload

---
+++ Ejemplos/Python/Corte2/mcd recursivo.py
@@ -0,0 +1,11 @@
+def mcd(a, b):
+    if a == 0 or b == 0:
+        return 0
+    if a == b:
+        return a
+    if a > b:
+        return mcd(a - b, b)
+    elif b > a:
+        return mcd(a, b - a)
+
+print(mcd(17,12))
