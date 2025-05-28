9afb4c6114352d5c6c6a948e51e822d74d59b6de
Ricardo Pino <119005597+RichxrdPino@users.noreply.github.com>
2025-05-28 10:18:47 -0500

    Update main.py

--- Trabajos/'Chess' Derivagos/main.py
+++ Trabajos/'Chess' Derivagos/main.py
@@ -23,39 +23,58 @@ for i in range(FILAS):
 print("Matriz Tablero")
 print(mTablero)
 
-# -------------------Función Dibujar Tablero----------------- #
+# -------------------Función Dibujar Tablero y Selección de pieza----------------- #
 
-def dibujarTablero(A):
+def dibujarTablero(A, seleccion=None):
     filas, cols = A.shape
     for i in range(filas):
         for j in range(cols):
-            pygame.draw.rect(pantalla, WHITE if (i + j) % 2 == 0 else BLACK, pygame.Rect(i*TAM_CASILLA + DESFASE_LADO, j*TAM_CASILLA + DESFASE_LADO, TAM_CASILLA, TAM_CASILLA))
+            rect = pygame.Rect(i*TAM_CASILLA + DESFASE_LADO, j*TAM_CASILLA + DESFASE_LADO, TAM_CASILLA, TAM_CASILLA)
+            
+            if seleccion == (j, i):
+                pygame.draw.rect(pantalla, AMARILLO, rect)
+                pygame.draw.rect(pantalla, AMARILLO_OSCURO, rect, 3)
+            else:
+                # coloreado normal (ejemplo: chessboard)
+                pygame.draw.rect(pantalla, WHITE if (i + j) % 2 == 0 else BLACK, rect)
 
+# -------------------Función Mover Pieza ----------------- #
+
+def moverPieza(A, pos1, pos2):
+    print(" :( ")
+    
 # -------------------Función principal----------------- #
 
 def main():
+    global seleccion, selecPieza
     running = True
     while running:
-        # Manejo de eventos
+        # 1) Manejo de eventos
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
-        # Lógica del juego
-        # TODO: actualizar objetos, detectar colisiones, etc.
-
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                seleccion = None # Reinicializar seleccion
+                selecPieza = False
+                x, y = event.pos
+                col = (x - DESFASE_LADO) // TAM_CASILLA
+                fila = (y - DESFASE_LADO) // TAM_CASILLA
+                # comprobamos que esté dentro del tablero
+                for area in posPiezas:
+                    print(area.collidepoint(event.pos))
+                    if area.collidepoint(event.pos):
+                        seleccion = (fila, col)
+                        selecPieza = True
         # Dibujado
-        pantalla.fill((20,20,20))
-        # TODO: dibujar objetos, UI, etc.
-        dibujarTablero(mTablero)
+        pantalla.fill((20, 20, 20))
+        dibujarTablero(mTablero, seleccion)
+        posPiezas.clear()
         dibujarPiezas(mPiezas)
-        
-        # Actualizar pantalla
+
         pygame.display.flip()
-        # Controlar FPS
         clock.tick(FPS)
 
-    # Salida limpia
     pygame.quit()
     sys.exit()
 
