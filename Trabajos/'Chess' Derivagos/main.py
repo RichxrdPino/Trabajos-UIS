77535721f27fbda1881ad36fb2267f1f27364f3c
Ricardo Pino <119005597+RichxrdPino@users.noreply.github.com>
2025-05-31 16:14:49 -0500

    Update main.py

--- Trabajos/'Chess' Derivagos/main.py
+++ Trabajos/'Chess' Derivagos/main.py
@@ -5,10 +5,29 @@ import numpy as np
 
 # Inicialización de Pygame
 pygame.init()
+pygame.mixer.init()
 
 # Configuración de la ventana
 pantalla = pygame.display.set_mode((ANCHO, ALTO))
 pygame.display.set_caption('Ajedrez Derivagos')
+
+# Fuente de un texto
+fuente = pygame.font.SysFont("Arial", 24)
+# Textos para mostrar los puntos
+textoPntsBlanco = fuente.render(f"+{pntB}", True, WHITE)
+textoPntsNegro = fuente.render(f"+{pntN}", True, WHITE)
+# Posición de los textos
+pos_textoPntsBlanco = (0,ALTO - 54)
+pos_textoPntsNegro = (0,20)
+
+# Sonido mover pieza
+sndMoverPieza = pygame.mixer.Sound("assets/sonidos/moverPieza.mp3")
+sndMoverPieza.set_volume(0.1)
+
+# Música de fondo
+pygame.mixer.music.load("assets/sonidos/musicaFondo.mp3")
+pygame.mixer.music.set_volume(0.02)
+pygame.mixer.music.play(-1)
 
 # Reloj para controlar FPS
 clock = pygame.time.Clock()
@@ -37,11 +56,20 @@ def dibujarTablero(A, seleccion=None):
             else:
                 # coloreado normal (ejemplo: chessboard)
                 pygame.draw.rect(pantalla, WHITE if (i + j) % 2 == 0 else BLACK, rect)
+                
+def moverPieza2(pieza2):
+    global pntB, pntN, turno
+    if turno == "blanco":
+        pntB = pntB + int(pieza2)
+    else:
+        pntN = pntN + int(pieza2)
+    print(f"Blancas: +{pntB}")
+    print(f"Negras: +{pntN}")
 
 # -------------------Función Mover Pieza ----------------- #
 
 def moverPieza(A):
-    global reyInicialB, torreInicial_1B, torreInicial_2B, reyInicialN, torreInicial_1N, torreInicial_2N
+    global reyInicialB, torreInicial_1B, torreInicial_2B, reyInicialN, torreInicial_1N, torreInicial_2N, pntB, pntN
     x, y = pos1
     z, w = pos2
     
@@ -49,185 +77,217 @@ def moverPieza(A):
     pieza = A[x, y]
     
     try:
-        if A[z,w] == 0:
-            # Movimiento peones negros (1.0)
-            if pieza == 1.0:
+        # Movimiento peones negros (1.0)
+        if pieza == 1.0:
+            # Si está vacío
+            if A[z,w] == 0:
                 # Un paso adelante
                 if z == x + 1 and y == w:
+                    moverPieza2(A[z, w])
                     A[z, w] = pieza
                     A[x, y] = 0
                     return True
                 # Dos pasos desde posición inicial
                 elif z == x + 2 and y == w and x == 1 and A[x+1,y] == 0:
+                    moverPieza2(A[z, w])
                     A[z, w] = pieza
                     A[x, y] = 0
                     return True
-            # Movimiento peones blancos (1.1)
-            elif pieza == 1.1:
+            else:
+                # Captura diagonal
+                if (z == x + 1 and w == y + 1) or (z == x + 1 and w == y - 1):
+                    moverPieza2(A[z, w])
+                    A[z, w] = pieza
+                    A[x, y] = 0
+                    return True
+
+        # Movimiento peones blancos (1.1)
+        elif pieza == 1.1:
+            if A[z,w] == 0:
                 # Un paso adelante
                 if z == x - 1 and y == w:
+                    moverPieza2(A[z, w])
                     A[z, w] = pieza
                     A[x, y] = 0
                     return True
                 # Dos pasos desde posición inicial
                 elif z == x - 2 and y == w and x == 6 and A[x-1,y] == 0:
+                    moverPieza2(A[z, w])
                     A[z, w] = pieza
                     A[x, y] = 0
                     return True
-            # Movimiento caballo blanco y negro
-            elif pieza == 2.0 or pieza == 2.1:
-                if ((z == x + 2 or z == x - 2) and (w == y + 1 or w == y - 1)) or ((z == x + 1 or z == x - 1) and (w == y + 2 or w == y - 2)):
+            else:
+                # Captura diagonal
+                if (z == x - 1 and w == y + 1) or (z == x - 1 and w == y - 1):
+                    moverPieza2(A[z, w])
                     A[z, w] = pieza
                     A[x, y] = 0
                     return True
-            # Movimiento de alfil negro
-            if A[x, y] == 3.0:
-                if abs(z - x) == abs(w - y):  # Movimiento diagonal
-                    dx = 1 if z > x else -1
-                    dy = 1 if w > y else -1
-                    libre = True
-                    for i in range(1, abs(z - x)):
-                        if A[x + i * dx, y + i * dy] != 0:
-                            libre = False
-                            break
-                    if libre:
-                        A[z, w] = A[x, y]
-                        A[x, y] = 0
-                        return True
-
-            # Movimiento de alfil blanco
-            if A[x, y] == 3.1:
-                if abs(z - x) == abs(w - y):  # Movimiento diagonal
-                    dx = 1 if z > x else -1
-                    dy = 1 if w > y else -1
-                    libre = True
-                    for i in range(1, abs(z - x)):
-                        if A[x + i * dx, y + i * dy] != 0:
-                            libre = False
-                            break
-                    if libre:
-                        A[z, w] = A[x, y]
-                        A[x, y] = 0
-                        return True
-            
-            # Movimiento de torre negra
-            if A[x, y] == 4.0:
-                if x == z or y == w:
-                    dx = 0 if x == z else (1 if z > x else -1)
-                    dy = 0 if y == w else (1 if w > y else -1)
-                    libre = True
-                    i = 1
-                    while (x + i*dx != z or y + i*dy != w):
-                        if A[x + i*dx, y + i*dy] != 0:
-                            libre = False
-                            break
-                        i += 1
-                    
-                    if libre:
-                        A[z, w] = A[x, y]
-                        A[x, y] = 0
-                        if (x == 0 and y == 0):
-                            torreInicial_1N = False
-                        elif (x == 0 and y == 7):
-                            torreInicial_2N = False
-                        return True
-
-            # Movimiento de torre blanca
-            if A[x, y] == 4.1:
-                if x == z or y == w:
-                    dx = 0 if x == z else (1 if z > x else -1)
-                    dy = 0 if y == w else (1 if w > y else -1)
-                    libre = True
-                    i = 1
-                    while (x + i*dx != z or y + i*dy != w):
-                        if A[x + i*dx, y + i*dy] != 0:
-                            libre = False
-                            break
-                        i += 1
-                    if libre:
-                        A[z, w] = A[x, y]
-                        A[x, y] = 0
-                        if (x == 7 and y == 0):
-                            torreInicial_1B = False
-                        elif (x == 7 and y == 7):
-                            torreInicial_2B = False
-                        return True
-            
-            # Movimiento de rey negro
-            if A[x, y] == 6.0:
-                if abs(z - x) <= 1 and abs(w - y) <= 1:
-                    A[z, w] = A[x, y]
+        # Movimiento caballo blanco y negro
+        if pieza == 2.0 or pieza == 2.1:
+            if ((z == x + 2 or z == x - 2) and (w == y + 1 or w == y - 1)) or ((z == x + 1 or z == x - 1) and (w == y + 2 or w == y - 2)):
+                moverPieza2(A[z, w])
+                A[z, w] = pieza
+                A[x, y] = 0
+                return True
+        # Movimiento de alfil negro
+        if A[x, y] == 3.0:
+            if abs(z - x) == abs(w - y):  # Movimiento diagonal
+                dx = 1 if z > x else -1
+                dy = 1 if w > y else -1
+                libre = True
+                for i in range(1, abs(z - x)):
+                    if A[x + i * dx, y + i * dy] != 0:
+                        libre = False
+                        break
+                if libre:
+                    moverPieza2(A[z, w])
+                    A[z, w] = pieza
                     A[x, y] = 0
-                    reyInicialN = False
                     return True
+
+        # Movimiento de alfil blanco
+        if A[x, y] == 3.1:
+            if abs(z - x) == abs(w - y):  # Movimiento diagonal
+                dx = 1 if z > x else -1
+                dy = 1 if w > y else -1
+                libre = True
+                for i in range(1, abs(z - x)):
+                    if A[x + i * dx, y + i * dy] != 0:
+                        libre = False
+                        break
+                if libre:
+                    moverPieza2(A[z, w])
+                    A[z, w] = pieza
+                    A[x, y] = 0
+                    return True
+        
+        # Movimiento de torre negra
+        if A[x, y] == 4.0:
+            if x == z or y == w:
+                dx = 0 if x == z else (1 if z > x else -1)
+                dy = 0 if y == w else (1 if w > y else -1)
+                libre = True
+                i = 1
+                while (x + i*dx != z or y + i*dy != w):
+                    if A[x + i*dx, y + i*dy] != 0:
+                        libre = False
+                        break
+                    i += 1
                 
-                # Lógica del enrroque
-                elif (z == x and w == y + 2) and A[x,y+1] == 0 and torreInicial_2N and reyInicialN:
-                    A[z, w] = A[x, y]
+                if libre:
+                    moverPieza2(A[z, w])
+                    A[z, w] = pieza
                     A[x, y] = 0
-                    A[x,y+1] = A[x,y+3]
-                    A[x,y+3] = 0
+                    if (x == 0 and y == 0):
+                        torreInicial_1N = False
+                    elif (x == 0 and y == 7):
+                        torreInicial_2N = False
                     return True
-                elif ((z == x and w == y - 2) or (z == x and w == y - 3)) and A[x,y-1] == 0 and A[x,y-2] == 0 and torreInicial_1N and reyInicialN:
-                    A[x, y-2] = A[x, y]
-                    A[x, y] = 0
-                    A[x,y-1] = A[x,y-4]
-                    A[x,y-4] = 0
-                    return True
-            # Movimiento de rey blanco
-            if A[x, y] == 6.1:
-                if abs(z - x) <= 1 and abs(w - y) <= 1:
-                    A[z, w] = A[x, y]
-                    A[x, y] = 0
-                    reyInicialB = False
-                    return True
-                
-                # Lógica del enrroque
-                elif (z == x and w == y + 2) and A[x,y+1] == 0 and torreInicial_2B and reyInicialB:
-                    A[z, w] = A[x, y]
-                    A[x, y] = 0
-                    A[x,y+1] = A[x,y+3]
-                    A[x,y+3] = 0
-                    return True
-                elif ((z == x and w == y - 2) or (z == x and w == y - 3)) and A[x,y-1] == 0 and A[x,y-2] == 0 and torreInicial_1B and reyInicialB:
-                    A[x, y-2] = A[x, y]
-                    A[x, y] = 0
-                    A[x,y-1] = A[x,y-4]
-                    A[x,y-4] = 0
-                    return True
-            # Movimiento de dama negra
-            if A[x, y] == 5.0:
-                if abs(z - x) == abs(w - y) or x == z or y == w:
-                    dx = 0 if x == z else (1 if z > x else -1)
-                    dy = 0 if y == w else (1 if w > y else -1)
-                    libre = True
-                    i = 1
-                    while (x + i*dx != z or y + i*dy != w):
-                        if A[x + i*dx, y + i*dy] != 0:
-                            libre = False
-                            break
-                        i += 1
-                    if libre:
-                        A[z, w] = A[x, y]
-                        A[x, y] = 0
-                        return True
 
-            # Movimiento de dama blanca
-            if A[x, y] == 5.1:
-                if abs(z - x) == abs(w - y) or x == z or y == w:
-                    dx = 0 if x == z else (1 if z > x else -1)
-                    dy = 0 if y == w else (1 if w > y else -1)
-                    libre = True
-                    i = 1
-                    while (x + i*dx != z or y + i*dy != w):
-                        if A[x + i*dx, y + i*dy] != 0:
-                            libre = False
-                            break
-                        i += 1
-                    if libre:
-                        A[z, w] = A[x, y]
-                        A[x, y] = 0
-                        return True      
+        # Movimiento de torre blanca
+        if A[x, y] == 4.1:
+            if x == z or y == w:
+                dx = 0 if x == z else (1 if z > x else -1)
+                dy = 0 if y == w else (1 if w > y else -1)
+                libre = True
+                i = 1
+                while (x + i*dx != z or y + i*dy != w):
+                    if A[x + i*dx, y + i*dy] != 0:
+                        libre = False
+                        break
+                    i += 1
+                if libre:
+                    moverPieza2(A[z, w])
+                    A[z, w] = pieza
+                    A[x, y] = 0
+                    if (x == 7 and y == 0):
+                        torreInicial_1B = False
+                    elif (x == 7 and y == 7):
+                        torreInicial_2B = False
+                    return True
+        
+        # Movimiento de rey negro
+        if A[x, y] == 6.0:
+            if abs(z - x) <= 1 and abs(w - y) <= 1:
+                moverPieza2(A[z, w])
+                A[z, w] = pieza
+                A[x, y] = 0
+                reyInicialN = False
+                return True
+            
+            # Lógica del enrroque
+            elif (z == x and w == y + 2) and A[x,y+1] == 0 and torreInicial_2N and reyInicialN:
+                A[z, w] = pieza
+                A[x, y] = 0
+                A[x,y+1] = A[x,y+3]
+                A[x,y+3] = 0
+                torreInicial_2N = False
+                reyInicialN = False
+                return True
+            elif ((z == x and w == y - 2) or (z == x and w == y - 3)) and A[x,y-1] == 0 and A[x,y-2] == 0 and torreInicial_1N and reyInicialN:
+                A[x, y-2] = pieza
+                A[x, y] = 0
+                A[x,y-1] = A[x,y-4]
+                A[x,y-4] = 0
+                return True
+        # Movimiento de rey blanco
+        if A[x, y] == 6.1:
+            if abs(z - x) <= 1 and abs(w - y) <= 1:
+                moverPieza2(A[z, w])
+                A[z, w] = pieza
+                A[x, y] = 0
+                reyInicialB = False
+                return True
+            
+            # Lógica del enrroque
+            elif (z == x and w == y + 2) and A[x,y+1] == 0 and torreInicial_2B and reyInicialB:
+                A[z, w] = pieza
+                A[x, y] = 0
+                A[x,y+1] = A[x,y+3]
+                A[x,y+3] = 0
+                return True
+            elif ((z == x and w == y - 2) or (z == x and w == y - 3)) and A[x,y-1] == 0 and A[x,y-2] == 0 and torreInicial_1B and reyInicialB:
+                A[x, y-2] = pieza
+                A[x, y] = 0
+                A[x,y-1] = A[x,y-4]
+                A[x,y-4] = 0
+                return True
+        # Movimiento de dama negra
+        if A[x, y] == 5.0:
+            if abs(z - x) == abs(w - y) or x == z or y == w:
+                dx = 0 if x == z else (1 if z > x else -1)
+                dy = 0 if y == w else (1 if w > y else -1)
+                libre = True
+                i = 1
+                while (x + i*dx != z or y + i*dy != w):
+                    if A[x + i*dx, y + i*dy] != 0:
+                        libre = False
+                        break
+                    i += 1
+                if libre:
+                    moverPieza2(A[z, w])
+                    A[z, w] = pieza
+                    A[x, y] = 0
+                    return True
+
+        # Movimiento de dama blanca
+        if A[x, y] == 5.1:
+            if abs(z - x) == abs(w - y) or x == z or y == w:
+                dx = 0 if x == z else (1 if z > x else -1)
+                dy = 0 if y == w else (1 if w > y else -1)
+                libre = True
+                i = 1
+                while (x + i*dx != z or y + i*dy != w):
+                    if A[x + i*dx, y + i*dy] != 0:
+                        libre = False
+                        break
+                    i += 1
+                if libre:
+                    moverPieza2(A[z, w])
+                    A[z, w] = pieza
+                    A[x, y] = 0
+                    return True      
     except:
         print("Fuera del tablero")
     # Si no se movió la pieza, devolver false
@@ -236,7 +296,7 @@ def moverPieza(A):
 # -------------------Función principal----------------- #
 
 def main():
-    global seleccion, selecPieza, pos1, pos2, cont, turno
+    global seleccion, selecPieza, pos1, pos2, cont, turno, pntB, pntN
     running = True
     while running:
         # Manejo de eventos
@@ -282,16 +342,21 @@ def main():
                     pos1 = (-1,-1)
                     pos2 = (-1,-1)
                     cont = 1
-                print(pos1)
-                print(pos2)
         # Dibujado
         pantalla.fill((20, 20, 20))
         dibujarTablero(mTablero, seleccion)
         posPiezas.clear()
         dibujarPiezas(mPiezas)
         
+        # Mostrar puntos de cada jugador
+        textoPntsBlanco = fuente.render(f"+{pntB}", True, WHITE)
+        textoPntsNegro = fuente.render(f"+{pntN}", True, WHITE)
+        pantalla.blit(textoPntsBlanco, pos_textoPntsBlanco)
+        pantalla.blit(textoPntsNegro, pos_textoPntsNegro)
+        
         if pos1 != (-1,-1) and pos2 != (-1,-1):
             if moverPieza(mPiezas):
+                sndMoverPieza.play()
                 if turno == "blanco":
                     turno = "negro" 
                 else:
